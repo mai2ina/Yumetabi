a29c5d86ff7657c8481a06d126d10040345262af
mai2ina <marina040217@gmail.com>
2020-05-04 11:22:09 +0000

    Fixed get desk record for likes action.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,8 +71,7 @@ class UsersController < ApplicationController
   
   def likes
     @user = User.find(params[:id])
-    @desks = @user.desks.select(:id, :name)
-    #binding.pry
+    @desks = Desk.select(:id, :name)
     @favorites = @user.liked.page(params[:page])
     counts(@user)
   end
